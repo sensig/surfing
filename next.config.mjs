82a08fdd0d484835fc146b00df98e1560a27ffab
sensig <164438265+sensig@users.noreply.github.com>
2024-03-22 08:19:09 -0700

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['iowivkgrejrwddohzemm.supabase.co'],
-  },
-};
-
-export default nextConfig;
